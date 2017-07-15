2c6a24ee60439a45aec3a6c72e7f9b77bedb98e4
icemango <icemango@csie.io>
2017-07-16 02:16:32 +0800

    Add app.js as echo bot Add package.json

---
+++ app.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('dotenv').config({'path': 'config.env'});
+
+const bot = new TelegramBot(process.env.API_TOKEN, {polling: true});
+
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Received your message: ' + msg.text);
+});
